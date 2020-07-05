5cb8c49e8f89725f42d8d2213cf1b30a5e379a3f
shubhamjoshii245 <67687086+shubhamjoshii245@users.noreply.github.com>
2020-07-05 02:56:09 -0700

    Add files via upload

---
+++ threebutton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class threebutton : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Screen");
+    }
+    public void MasterScene()
+    {
+        SceneManager.LoadScene("MasterScene");
+    }
+}
