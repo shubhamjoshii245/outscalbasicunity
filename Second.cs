47f59b24cdd9f7ce707a85710895fd7cc3021f1d
shubhamjoshii245 <67687086+shubhamjoshii245@users.noreply.github.com>
2020-07-04 21:12:22 -0700

    Add files via upload

---
+++ Second.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Second.cs : MonoBehaviour
+{
+private int[] ids;
+
+    private void Awake()
+    {
+        Debug.Log("My scond code");
+    }
+
+
+    void Start()
+    {
+        Debug.Log("My second script towards learning");
+    }
+
+  
+    void Update()
+    {
+        Debug.Log("My second script towards learning");
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+    ids=null;
+    }
+}
